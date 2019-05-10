ecdadf54548b0ddc910c797e5ea3430eaff7a3e1
linvisf <linvisf>
2019-05-10 21:16:32 +0800

    给模型添加方法，以及给数据库添加数据

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,6 @@
+import datetime
+from datetime import timezone
+
 from django.db import models
 
 # 创建两个模型，Question 和 Choice
@@ -9,8 +12,17 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.choice_text
