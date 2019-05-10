50ddc72dc700c18c0853abfcce34937ede18530c
linvisf <linvisf>
2019-05-10 23:57:02 +0800

    1、创建公共界面-视图（4个）    主页、详情、结果、投票 2、使用模块 3、render() 4、引发404错误    快捷方法：get_object_or_404() 5、删除模板中硬编码url 6、命名空间Url名称

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,39 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
+from django.template import loader
+
+from polls.models import Question
+
+# 有四个视图：主页、详情、结果、投票
 
 
 def index(request):
-    return HttpResponse("Hello world. You're at the polls index")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # output = ', '.join([q.question_text for q in latest_question_list])
+    context = {'latest_question_list': latest_question_list}
+    # 第一种方法
+    # template = loader.get_template('polls/index.html')
+    # return HttpResponse(template.render(context, request))
+    # 第二种方法
+    return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id):
+    # 方法一 get()和http404
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+
+    # 方法二 get_object_or_404
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
