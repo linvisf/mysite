7186392db45d1a72d109f4661596880ad6fd35ff
linvisf <linvisf>
2019-05-11 17:54:45 +0800

    使用通用视图：更少代码 1、修改URLconf。 2、删除一些旧的，不需要的视图。 3、基于Django的通用视图引入新视图。

--- polls/views.py
+++ polls/views.py
@@ -2,40 +2,59 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.template import loader
 from django.urls import reverse
+from django.views import generic
 
 from polls.models import Question, Choice
 
 
 # 有四个视图：主页、详情、结果、投票
 
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    # output = ', '.join([q.question_text for q in latest_question_list])
-    context = {'latest_question_list': latest_question_list}
-    # 第一种方法
-    # template = loader.get_template('polls/index.html')
-    # return HttpResponse(template.render(context, request))
-    # 第二种方法
-    return render(request, 'polls/index.html', context)
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-    # 方法一 get()和http404
-    # try:
-    #     question = Question.objects.get(pk=question_id)
-    # except Question.DoesNotExist:
-    #     raise Http404("Question does not exist")
-
-    # 方法二 get_object_or_404
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+class DetailView(generic.DetailView):
+    template_name = 'polls/detail.html'
+    model = Question
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class ResultsView(generic.DetailView):
+    template_name = 'polls/results.html'
+    model = Question
 
+
+# 删除下面代码（上面内容即是优化内容）
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     # output = ', '.join([q.question_text for q in latest_question_list])
+#     context = {'latest_question_list': latest_question_list}
+#     # 第一种方法
+#     # template = loader.get_template('polls/index.html')
+#     # return HttpResponse(template.render(context, request))
+#     # 第二种方法
+#     return render(request, 'polls/index.html', context)
+#
+#
+# def detail(request, question_id):
+#     # 方法一 get()和http404
+#     # try:
+#     #     question = Question.objects.get(pk=question_id)
+#     # except Question.DoesNotExist:
+#     #     raise Http404("Question does not exist")
+#
+#     # 方法二 get_object_or_404
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/detail.html', {'question': question})
+#
+#
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {'question': question})
+#
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
